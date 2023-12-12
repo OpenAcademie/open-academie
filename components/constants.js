830bca1c54bca44054d6d47825dc4754c21eea19
L'intendant zonard - Etienne <etienne@intendancezone.net>
2023-12-12 11:05:32 +0100

    Mise en prod CédiSCO 2.0.3

--- components/constants.js
+++ components/constants.js
@@ -98,11 +98,11 @@ export const Products = [
     {
     id: 'cedisco',
     page: '/cedisco',
-    title: 'CédiSCO 2.0.1',
+    title: 'CédiSCO 2.0.3',
     formUrl: 'https://docs.google.com/forms/d/1AcTYcJrqYufC7N8SUeWzC1EpID0kprPHjTNq8Z5JOuk/formResponse?entry.1936573090=',
     emailEntryRef: '843103382',
     pageEntryRef: '89190430',
-    downloadUrl: '/static/files/CédiSCO-2-0-1.zip',
+    downloadUrl: '/static/files/CédiSCO-2-0-3.zip',
     tagline: 'Les conseils de discipline sans souci de procédure',
     description: 'CédiSCO assure l\'ensemble du travail de procédure pour le conseil de discipline, sans risque d\'erreur et sans effort.',
     imageSource: '/static/balance.png',
